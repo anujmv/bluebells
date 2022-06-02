6043f4a715446563e55f68a9aff3737364308aad
Pingcube <pingcube@Pingcubes-MacBook-Air.local>
2022-06-02 22:18:18 +0530

    testimonials

--- src/index.js
+++ src/index.js
@@ -18,6 +18,44 @@ $(document).ready(function () {
     pauseOnHover: false,
     pauseOnFocus: false,
   });
+  $(".slick-testimonial").slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
 });
 
 $(window).on("scroll", function () {
